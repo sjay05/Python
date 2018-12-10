bd044b351cf1da04503cb6b5c468bfaaa0c24722
sjay05 <41878734+sjay05@users.noreply.github.com>
2018-12-10 07:46:53 -0500

    Add files via upload

--- CCC/2012CCC/J1.py
+++ CCC/2012CCC/J1.py
@@ -1,14 +1,13 @@
-x = input('Enter the speed limit: ')
-y = input('Enter recorded speed of the car: ')
+x = input()
+y = input()
 
 if y <= x:
-    print "Congratulations, you are whitin the speed limit!"
+    print "Enter the speed limit: Enter the recorded speed of the car: Congratulations, you are within the speed limit!"
 elif y > x and y - x in range(1, 21):
-    f = '$100'
-    print 'You are speeding and your fine is %s' %f
+    print 'Enter the speed limit: Enter the recorded speed of the car: You are speeding and your fine is $100.'
 elif y > x and y - x in range(21, 31):
-    f = '$270'
-    print 'You are speeding and your fine is %s' %f
+    print 'Enter the speed limit: Enter the recorded speed of the car: You are speeding and your fine is $270.'
 elif y > x and y - x >= 31:
-    f = '$500'
-    print 'You are speeding and your fine is %s' %f
+    print 'Enter the speed limit: Enter the recorded speed of the car: You are speeding and your fine is $500.'
+
+
