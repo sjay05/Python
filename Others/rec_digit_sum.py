02dca4f5c8e8fbc85da5397dd6b4037424ebadcc
sjay05 <anusha.perumal@gmail.com>
2019-02-06 09:33:27 -0500

    Two programs that use recursion and iteration to calculate digit sum multiple times until value equals 1.

---
+++ Others/rec_digit_sum.py
@@ -0,0 +1,19 @@
+n = 123
+k = 3
+
+# recursive function to to calculate sum of digits multiple times until value == 1.
+def digit_sum(value):
+    if len(str(value)) == 1:
+        print value
+    else:
+        value = int_sum(int(value))
+        digit_sum(value)
+
+# recursvive function to calculate sum of digits
+def int_sum(number):
+    if len(str(number)) == 1:
+        return number
+    else:
+        return int(str(number)[0]) + int_sum(int(str(number)[1:]))
+
+digit_sum(str(n)*k)
